e44f5a43f97b0f13cf3699087bdd54be4e3f9432
carlinaylsworth <carlinaylsworth@hotmail.com>
2015-09-22 19:48:58 -0700

    Fixed issue with redirects
    Removed whitespace from top of .php file

--- CMS-DEMO/public/delete_subject.php
+++ CMS-DEMO/public/delete_subject.php
@@ -2,7 +2,6 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 <?php confirm_logged_in(); ?>
-
 <?php
 	$current_subject = find_subject_by_id($_GET["subject"], false);
 	if (!$current_subject) {
@@ -29,6 +28,5 @@
 		// Failure
 		$_SESSION["message"] = "Subject deletion failed.";
 		redirect_to("manage_content.php?subject={$id}");
-	}
-	
+	}	
 ?>
