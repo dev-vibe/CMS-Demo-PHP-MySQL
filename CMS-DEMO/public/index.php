0610e3a1ad9cbf52d957a5d0f6910508f802d25b
carlinaylsworth <carlinaylsworth@hotmail.com>
2015-09-26 01:37:51 -0700

    Revert "Put user panel into function.php"
    This reverts commit 8e7e0458480efd794a7a1ac254862059cecfb128.

--- CMS-DEMO/public/index.php
+++ CMS-DEMO/public/index.php
@@ -10,7 +10,15 @@
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
           <!-- Sidebar user panel (optional) -->
-		  <?php if (logged_in()) { echo userPanel(); } ?>
+          <div class="user-panel">
+            <div class="pull-left image">
+              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+              <!-- Status -->
+              <?php if (logged_in()) { echo "<br><a href=\"#\"><i class=\"fa fa-circle text-success\"></i> Logged in</a>"; } ?>
+            </div>
+          </div>
           <!-- Sidebar Menu -->
           <?php echo public_navigation($current_subject, $current_page); ?>
           <!-- /.sidebar-menu -->
